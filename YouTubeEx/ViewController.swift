34423289d9889ea76600545281a205d528d6d541
user140592 <user140592@eu201.local>
2019-01-03 19:57:40 +0000

    Initial Commit

---
+++ YouTubeEx/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YouTubeEx
+//
+//  Created by user140592 on 1/3/19.
+//  Copyright © 2019 talspektor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
